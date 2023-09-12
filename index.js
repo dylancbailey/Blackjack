475d28ef5d10fa5f2764a37995f1452f412fdb43
Dylan Bailey <wearesleepless@gmail.com>
2023-09-12 11:38:31 -0400

    Basic logic

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 let firstCard = 10
-let secondCard = 12
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 // Functions
 let startGame = () => {
+    sumEl.textContent += sum
+    cardsEl.textContent = "Cards: " + firstCard + " / " + secondCard
+
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Woohoo! You got Blackjack!"
+        message = "You got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game :("
+        message = "You're out of the game."
         isAlive = false
     }
 
-    console.log(message)
+    messageEl.textContent = message
 }
