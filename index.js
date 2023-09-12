314d0f5c8b8abf53c7e965775380ce9e7a74b507
Dylan Bailey <wearesleepless@gmail.com>
2023-09-12 12:53:06 -0400

    Added more functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 11
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -10,8 +11,12 @@ let cardsEl = document.querySelector("#cards-el")
 
 // Functions
 let startGame = () => {
-    sumEl.textContent += sum
-    cardsEl.textContent = "Cards: " + firstCard + " / " + secondCard
+    renderGame()
+}
+
+let renderGame = () => {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + cards[0] + " / " + cards[1]
 
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -22,6 +27,11 @@ let startGame = () => {
         message = "You're out of the game."
         isAlive = false
     }
-
     messageEl.textContent = message
+}
+
+let newCard = () => {
+    let card = 7
+    sum += card
+    renderGame() 
 }
