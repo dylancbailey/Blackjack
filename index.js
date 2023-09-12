d301fa25707def2575cab3e6277d95fae52d5376
Dylan Bailey <wearesleepless@gmail.com>
2023-09-12 11:19:53 -0400

    Button

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Woohoo! You got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game :("
-    isAlive = false
-}
+// Functions
+let startGame = () => {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Woohoo! You got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game :("
+        isAlive = false
+    }
 
-console.log(message)
+    console.log(message)
+}
